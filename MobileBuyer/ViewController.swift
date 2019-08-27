45be93692f7abdb1ebc1bfe208796e752943489e
chawalya tantisevi <chawalyaa@gmail.com>
2019-08-27 11:41:45 +0700

    Initial Commit

---
+++ MobileBuyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileBuyer
+//
+//  Created by Chawalya Tantisevi on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
